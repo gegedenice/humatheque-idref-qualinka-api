5fc9444d8d11a112891f3ecca65eb72acdc1510c
Géraldine Geoffroy <grldn.geoffroy@gmail.com>
2026-04-30 17:50:27 +0200

    hardcode port

--- app.py
+++ app.py
@@ -729,5 +729,4 @@ async def align_person_endpoint(
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=8001)

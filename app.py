76b7f2ffadf9ca10d503a2ca7fe44fc11dbb5396
gegedenice <grldn.geoffroy@gmail.com>
2026-09-10 14:50:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1133,5 +1133,4 @@ async def align_thesis_organizations_endpoint(
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=8001)
